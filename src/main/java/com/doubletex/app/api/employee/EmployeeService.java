d9f01c24a958a68298e63876dbac9ea6c2690f12
Alexandru-Paul Enache <alexandru.enache@visma.com>
2022-06-09 13:34:59 +0300

    lab-10 conflicts

--- src/main/java/com/doubletex/app/api/employee/EmployeeService.java
+++ src/main/java/com/doubletex/app/api/employee/EmployeeService.java
@@ -62,7 +62,7 @@ public class EmployeeService {
 
     public Page<Employee> search(Integer pageNumber, Integer pageSize, String sortBy, String name) {
         Pageable pageable = PageRequest.of(pageNumber, pageSize, Sort.by(sortBy));
-        return employeeRepository.findEmployeesByNameLike(name, pageable);
+        return employeeRepository.findEmployeesByFullNameLike(pageable, name);
     }
 
 }
